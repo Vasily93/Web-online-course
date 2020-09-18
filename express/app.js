3179c5085e31a5973c69274ba74397013454e32a
Vasily93 <markovavasily@gmail.com>
2020-09-17 21:19:08 -0700

    explored routes params

--- express/app.js
+++ express/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
 
+//Routes
+//Order of routes matters!!
+//req.params = object of nodes from url
 app.get('/', (req, res) => {
     res.send('hi vasily!')
 });
+
 app.get('/dog', (req, res) => {
     res.send('wooof!')
 });
+
 app.get('/bye', (req, res) => {
     res.send('goodbye Vasily!')
 });
 
+app.get('/vasya/:whatever', (req, res) => {
+    const params = req.params.whatever.toUpperCase();
+    res.send(`Welcome to ${params} dude`) // collon means any pattern after parent slash
+});
+
+app.get('*',(req, res) => {
+    res.send('you are a star!') //works for all undeclared routes
+});
+
+//Listening on port 3000
 app.listen(3000, () => {
     console.log('listening on port 3000')
-})
+});
