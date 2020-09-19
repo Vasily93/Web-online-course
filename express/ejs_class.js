10c7812bb3331d7bee36e7ee5ef18b4f9098aaee
Vasily93 <markovavasily@gmail.com>
2020-09-18 18:05:37 -0700

    finished with ejs

--- express/ejs_class.js
+++ express/ejs_class.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.render('home.ejs')
 });
@@ -9,6 +11,16 @@ app.get('/admin/:name', (req, res) => {
     const name = req.params.name;
     res.render('admin.ejs', {nameVar: name})
 });
+
+app.get('/posts', (req, res) => {
+    const posts = [
+        {title:'cats', author:'jols'},
+        {title:'dogs', author:'bolh'},
+        {title:'turtles', author:'vals'}
+    ];
+    res.render('posts.ejs', {postsArray: posts});
+});
+
 
 app.listen('3000', () => {
     console.log('Listening on port 3000')
